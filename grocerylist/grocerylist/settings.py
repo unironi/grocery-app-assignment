183dc31f32a307c7d83af596d86f72b00a37d5c3
Oroni Hasan <o3hasan@uwaterloo.ca>
2026-01-14 22:39:07 -0500

    fix in settings.py

--- grocerylist/grocerylist/settings.py
+++ grocerylist/grocerylist/settings.py
@@ -90,7 +90,9 @@ WSGI_APPLICATION = 'grocerylist.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
     }
 }
 
